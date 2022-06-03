bd01610b8be96e67ad83025dbfb3d00888ade281
Twan Goosen <twan@clarin.eu>
2022-06-03 17:47:59 +0300

    exercise 2

--- tutorial/common.py
+++ tutorial/common.py
@@ -1,2 +1,5 @@
+from os.path import expanduser
+data_dir = expanduser("~") + '/work/data'
+
 nsmap = {"cmd": "http://www.clarin.eu/cmd/1",
          "cmdp_text": "http://www.clarin.eu/cmd/1/profiles/clarin.eu:cr1:p_1633000337997"}
