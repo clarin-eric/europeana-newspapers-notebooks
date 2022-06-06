321b9c196bd09234b758a7ac2a05aacfe6096d2c
Twan Goosen <twan@clarin.eu>
2022-06-06 13:44:42 +0300

    data dir: ~/data

--- tutorial/common.py
+++ tutorial/common.py
@@ -1,5 +1,5 @@
 from os.path import expanduser
-data_dir = expanduser("~") + '/work/data'
+data_dir = expanduser("~") + '/data'
 
 nsmap = {"cmd": "http://www.clarin.eu/cmd/1",
          "cmdp_text": "http://www.clarin.eu/cmd/1/profiles/clarin.eu:cr1:p_1633000337997"}
